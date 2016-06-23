bed9337480e76902baae7fa1d0b0a63ca8f18335
Gabe Boning <gboning@gmail.com>
2016-06-23 15:40:01 +0200

    animation between pages

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var app = angular.module("FinalApp", ["ngRoute", "sticky"]);
+var app = angular.module("FinalApp", ["ngRoute", "sticky", "ngAnimate"]);
 
 app.config(function($routeProvider) {
   $routeProvider.when("/week1", {
     templateUrl: "templates/week1.html"
+  });
+  $routeProvider.when("/week2", {
+    templateUrl: "templates/week2.html"
   });
   $routeProvider.when("/memories", {
     templateUrl: "templates/memories.html"
@@ -14,6 +17,7 @@ app.config(function($routeProvider) {
 app.controller("AppCtrl", function($scope, $rootScope, $location) {
   $rootScope.$on('$routeChangeSuccess', function(e, current, pre) {
     $scope.route = $location.path().substring(1);
+    $(".main-wrap").velocity("scroll", {offset: -90});
   });
   
 });
